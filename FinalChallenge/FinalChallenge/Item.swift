7c5282dc1e65fcd97401586920815abe8c4ac4ea
Pamella <pamella.as.304@gmail.com>
2024-09-25 08:57:23 -0300

    created the first file

---
+++ FinalChallenge/FinalChallenge/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FinalChallenge
+//
+//  Created by Pamella Alvarenga on 23/09/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
